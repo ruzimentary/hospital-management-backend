e355e079d3f66a42bff41197f59d452b43be23d3
ruzimentary <wanyonyi_andrew@yahoo.com>
2024-07-11 19:45:54 +0300

    adding routes

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_migrate import Migrate
-from models import db, Doctor, Department
+from models import db, Doctor, Department, Patient, Appointment,  Medication, Medicalrecords
 
 app = Flask(__name__)
 
@@ -24,6 +24,26 @@ def get_all_doctors():
 def get_all_departments():
     departments = Department.query.all()
     return jsonify([department.to_dict() for department in departments])
+
+@app.route('/patients', methods=['GET'])
+def get_all_patients():
+    patients = Patient.query.all()
+    return jsonify([patient.to_dict() for patient in patients])
+
+@app.route('/appointments', methods=['GET'])
+def get_all_appointments():
+    appointments = Appointment.query.all()
+    return jsonify([appointment.to_dict() for appointment in appointments])
+
+@app.route('/medications', methods=['GET'])
+def get_all_medications():
+    medications = Medication.query.all()
+    return jsonify([medication.to_dict() for medication in medications])
+
+@app.route('/medicalrecords', methods=['GET'])
+def get_all_medicalrecords():
+    medications = Medicalrecords.query.all()
+    return jsonify([medication.to_dict() for medication in medications])
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
